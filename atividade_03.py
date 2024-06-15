89f1a280b87742b18efcac0b5e97ddbca8a8725f
Juliana Azevedo <julianaazevedo280@gmail.com>
2024-06-15 14:39:01 -0300

    Semana 03_on33

---
+++ atividade_03.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome: ")
+nota = float(input("Digite sua nota: "))
+presencatotal = 100
+aulasfrequentadas = float(input("Digite sua frequência: "))
+presenca =  aulasfrequentadas / presencatotal * 100
+print(float(presenca))
+if nota >= 7 and presenca >= 75:
+    print ("Aprovada")
+else:
+    print ("Reprovada")
